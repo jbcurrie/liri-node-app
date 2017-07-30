4ba67aba352ebe6e68b40863b852c234f2a27730
jbcurrie <biobacker30@gmail.com>
2017-07-30 00:00:28 -0400

    files

---
+++ keys.js
@@ -0,0 +1,16 @@
+
+// // 3. Make a JavaScript file named `keys.js`. 
+// **Do Not** add this file to the .gitignore. 
+// This would be a good thing to do in the real world, but it makes grading this assignment a challenge.
+
+// Inside keys.js your file will look like this:
+
+
+// console.log('this is loaded');
+
+// exports.twitterKeys = {
+//   consumer_key: '<input here>',
+//   consumer_secret: '<input here>',
+//   access_token_key: '<input here>',
+//   access_token_secret: '<input here>',
+// }

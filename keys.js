29bbd5c06f9d424cf2f7f3f5047a5b6f58b03b14
J_Be_C <jbcurrie@users.noreply.github.com>
2017-09-06 01:27:25 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,11 +1,3 @@
-
-// // 3. Make a JavaScript file named `keys.js`. 
-// **Do Not** add this file to the .gitignore. 
-// This would be a good thing to do in the real world, but it makes grading this assignment a challenge.
-
-// Inside keys.js your file will look like this:
-
-
 console.log('this is loaded');
 
 exports.twitterKeys = {
